06b6c214f838c75499281f094bcd44d1e26b77e9
Pablo Cigoy <pablocigoy@gmail.com>
2022-12-15 16:53:55 +0100

    done

--- routes/recipe.routes.js
+++ routes/recipe.routes.js
@@ -51,7 +51,6 @@ router.get('/pages/CRUD/details', (req, res) => {
 // likes
 router.put('/pages/CRUD/:id/likes', (req, res, next) => {
     const [ ...likes]  = req.body
-    console.log(likes)
     Recipe.findByIdAndUpdate(req.params.id, {
     likes
     })
@@ -80,61 +79,60 @@ router.get("/pages/CRUD/:id", (req, res) => {
 
 
 //edit recipe get
-
-router.get("/pages/CRUD/:id/edit", async (req, res) => {
-    const id = req.params.id
-    Recipe.findById(id)
-    try {
-        const recipe = await Recipe.findById(id)
-        // console.log(recipe)
-        res.json("pages/CRUD/edit", { user: req.session.user, recipe })
-    } catch (err) {
-        console.log(err)
-    }
-})
-// findByIdAndUpdate
+// router.get("/pages/CRUD/:id/edit", async (req, res) => {
+//     const id = req.params.id
+//     Recipe.findById(id)
+//     try {
+//         const recipe = await Recipe.findById(id)
+//         // console.log(recipe)
+//         res.json("pages/CRUD/edit", { user: req.session.user, recipe })
+//     } catch (err) {
+//         console.log(err)
+//     }
+// })
+// // findByIdAndUpdate
 
 
 
 //edit post recipe
-router.post("/pages/CRUD/:id",/*  uploader.single("Image"), */(req, res, next) => {
-    const id = req.params.id
-    const { name, region, type, image, time, service, ingredients, instructions, tips, reviews /* owner */ } = req.body
-    /*   const imgName = req.file.originalname
-      const imgPath = req.file.path
-      const publicId = req.file.filename   */
-    // console.log(req.body)
-    const recipe = {
-        name,
-        region,
-        type,
-        image,
-        time,
-        service,
-        ingredients,
-        instructions,
-        tips,
-        reviews,
-    }
-    Recipe.findById(id)
-        .then(data => {
-            if (data.owner._id.toString() !== req.session.user._id) {
-                console.log(err)
-                // res.json("recipe/rest", { user: req.session.user, recipe, message: "Oops! you can not Edit." })
-            } else {
-                Recipe.findByIdAndUpdate(id, recipe, { new: true })
+// router.post("/pages/CRUD/:id",/*  uploader.single("Image"), */(req, res, next) => {
+//     const id = req.params.id
+//     const { name, region, type, image, time, service, ingredients, instructions, tips, reviews /* owner */ } = req.body
+//     /*   const imgName = req.file.originalname
+//       const imgPath = req.file.path
+//       const publicId = req.file.filename   */
+//     // console.log(req.body)
+//     const recipe = {
+//         name,
+//         region,
+//         type,
+//         image,
+//         time,
+//         service,
+//         ingredients,
+//         instructions,
+//         tips,
+//         reviews,
+//     }
+//     Recipe.findById(id)
+//         .then(data => {
+//             if (data.owner._id.toString() !== req.session.user._id) {
+//                 console.log(err)
+//                 // res.json("recipe/rest", { user: req.session.user, recipe, message: "Oops! you can not Edit." })
+//             } else {
+//                 Recipe.findByIdAndUpdate(id, recipe, { new: true })
 
-                    .then(createdRecipe => {
-                        console.log(createdRecipe)
+//                     .then(createdRecipe => {
+//                         console.log(createdRecipe)
 
-                        res.redirect("/ProfilePage/ProfilePage")
-                    })
-            }
-        })
-        .catch(err => {
-            next(err)
-        })
-})
+//                         res.redirect("/ProfilePage/ProfilePage")
+//                     })
+//             }
+//         })
+//         .catch(err => {
+//             next(err)
+//         })
+// })
 
 
 
