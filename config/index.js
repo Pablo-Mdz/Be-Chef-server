39410f88d714c391d9a31c3b458c93b6f5875b9a
Pablo Cigoy <pablocigoy@gmail.com>
2022-12-25 12:12:39 +0100

    test2

--- config/index.js
+++ config/index.js
@@ -24,8 +24,8 @@ module.exports = (app) => {
     // controls a very specific header to pass headers from the frontend
     app.use(
         cors({
-            origin: "https://be-chef.netlify.app",
-            // origin: FRONTEND_URL,
+            // origin: "https://be-chef.netlify.app",
+            origin: FRONTEND_URL,
             credentials: true,
         })
     );
