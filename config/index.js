fa8d353c7b080620a55923dd14994ccf2c5c7922
Pablo Cigoy <pablocigoy@gmail.com>
2023-05-31 13:44:09 +0200

    changes2

--- config/index.js
+++ config/index.js
@@ -14,7 +14,7 @@ module.exports = (app) => {
     app.use(
         cors({
             // origin: "https://be-chef.netlify.app",
-            // origin: "https://be-chef.netlify.app",
+            
 
             origin: FRONTEND_URL,
             credentials: true,
